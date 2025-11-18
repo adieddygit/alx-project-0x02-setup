8b28e2ea5d4fe57e348c09026b9a79b4679cb266
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-18 05:23:32 +0000

    Project updated with 2 directories, pages/index.tsx updated with a welcome message

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import {FC} from 'react';
+
+const Home: FC = ()=>{
+  return(
+    <>
+      <Head>
+        <meta name='description' content='SoloForge: Mastering Project Autonomy'/>
+        <title>Next.js Project Setup and Basics</title>
+      </Head>
+
+      <main>
+        <div className='flex items-center justify-center h-screen m-4'>
+        <h1 className='text-4xl sm:text-4xl md:text-5xl font-extrabold'>Welcome, We are Glad to Have You </h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Home;
