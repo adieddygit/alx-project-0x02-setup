26b3914ede075c172770c4873b5c55c0b4022f86
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-19 00:47:49 +0000

    Created a reusable Card component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,24 @@
 export interface CardProps{
-    name: string;
-    address: string;
+  title: string;
+  content: string | React.ReactNode;
+//   subtitle?: string;
+//   description?: string;
+
+  // Media
+//   image?: string;
+//   imageAlt?: string;
+
+  // Actions
+//   href?: string;
+//   onClick?: () => void;
+//   buttonLabel?: string;
+
+  // Layout / Styling
+//   className?: string;
+//   rounded?: "sm" | "md" | "lg" | "xl" | "2xl" | "full";
+//   shadow?: boolean;
+
+  // Data (optional for dynamic content)
+//   tag?: string;
+//   badge?: string;
 }
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,49 @@
 import { FC} from 'react';
+import Card from '@/components/common/Card';
 
 const Home: FC = ()=>{
     return (
         <div>
-            <h1 className='flex items-center justify-center font-bold text-4xl h-screen'>This is the home page</h1>
+            <h1 className='flex items-center justify-center font-bold text-4xl shadow-md m-5 p-4'>This is the home page</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 m-4'>
+            <Card title='Beachfront Villa'
+            content={
+                
+                <ul className='list-disc ml-5'>
+                    <li>Stunning ocean views</li>
+                    <li>Private outdoor pool</li>
+                    <li>3–5 bedrooms</li>
+                    <li>Perfect for family vacations</li>
+                </ul>
+            }/>
+            <Card title='Standard Room'
+            content={
+                <ul className='list-disc ml-5'>
+                    <li>Fully furnished single room</li>
+                    <li>Comfortable bed</li>
+                    <li>Private bathroom</li>
+                    <li>Free Wi-Fi + ceiling fan</li>
+                </ul>
+            }/>
+            <Card title='Deluxe Apartment'
+            content={
+                <ul className='list-disc ml-5'>
+                    <li>Spacious 1–2 bedroom apartment</li>
+                    <li>Fully equipped kitchen</li>
+                    <li>Air-conditioned</li>
+                    <li>24/7 security & water supply</li>
+                </ul>
+            }/>
+            <Card title='Deluxe Apartment'
+            content={
+                <ul className='list-disc ml-5'>
+                    <li>Spacious 1–2 bedroom apartment</li>
+                    <li>Fully equipped kitchen</li>
+                    <li>Air-conditioned</li>
+                    <li>24/7 security & water supply</li>
+                </ul>
+            }/>
+            </div>
         </div>
     )
 }
