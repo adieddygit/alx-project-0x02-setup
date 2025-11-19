c185db4f95319a71d7fbc9017e65cdbb00d665b2
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-19 00:51:10 +0000

    Created a reusable Card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -2,14 +2,12 @@ import { CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ( {title, content})=>{
     return(
-        <>
         <div className="bg-white rounded-xl shadow-md p-6">
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
             <div className="text-gray-700">
                 {content}
             </div>
         </div>
-        </>
     )
 }
 
