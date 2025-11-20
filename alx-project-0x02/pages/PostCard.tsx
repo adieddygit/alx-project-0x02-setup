03b7f50a5dc1c637558f01582f7f543d4868014d
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-20 16:51:40 +0000

    Implemented the PostCard Component

--- alx-project-0x02/pages/PostCard.tsx
+++ alx-project-0x02/pages/PostCard.tsx
@@ -12,12 +12,6 @@ const PostCard: React.FC<PostProps> = ({title, content, userId})=>{
             </div>
         </div>
     )
-}
+};
 
-export async function getStaticProps(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const posts = await response.json();
-
-    return {props: {posts}};
-}
 export default PostCard;
