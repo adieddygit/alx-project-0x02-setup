6c82e44c5c9296be31889a463dee155a1a3a96ac
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-20 16:54:03 +0000

    Implemented the PostCard Component

--- alx-project-0x02/pages/PostCard.tsx
+++ alx-project-0x02/pages/PostCard.tsx
@@ -1,17 +1,15 @@
 import { PostProps } from "@/interfaces";
 
-const PostCard: React.FC<PostProps> = ({title, content, userId})=>{
-    return(
-        <div>
-            <div>
-                <h1>{title}</h1>
-            </div>
-            <div>{content}</div>
-            <div>
-                <span>User ID: {userId}</span>
-            </div>
-        </div>
-    )
-};
+const PostCard: React.FC<PostProps> = ({ title, content, userId }) => {
+  return (
+    <div className="border border-gray-200 rounded-lg shadow-md p-4 mb-4 max-w-sm hover:shadow-lg transition duration-300">
+      <h3 className="text-xl font-semibold mb-2 text-gray-800">{title}</h3>
+      <p className="text-sm text-gray-600 mb-4 whitespace-pre-wrap">
+        {content}
+      </p>
 
+      <span className="text-xs text-gray-500">{userId}</span>
+    </div>
+  );
+};
 export default PostCard;
